cf7f926eb83552fdab0f1ead1a9905493f85c56a
Nimrod Hajaj <hrodenticus@gmail.com>
2017-11-07 18:02:50 -0500

    Changed to linked list and modified spacing

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import numpy as np
 import random
+
+class Car: 
+  def __init__(self, position = 0, velocity = 0, prev = None, next = None): 
+    self.p = position
+    self.v = velocity
+    self.prev = prev    
+    self.next = next
+  def __str__(self): 
+    return str(position) + ", " + str(velocity)
+
 class traffic:
-    def __init__(self, n = 100, max_v = 5, density = 0.03):
-        self.n = n
-        self.road = np.zeros(n)
-        self.max_v = max_v
-        self.density = density
-        self.generate_car()
-        self.print_road()
+  def __init__(self, n = 100, max_v = 5, density = 0.03):
+    self.n = n
+    self.head = None
+    self.max_v = max_v
+    self.density = density
+    #self.generate_cars()
+    self.print_road()
 
-    def generate_car(self):
-        for i in range(self.n):
-            if(random.random() < self.density):
-                self.road[i] = 1
+  def iteration(self):
+    
 
-    # def iteration(self):
+  def generate_cars(self):
+    for i in range(self.n):
+      if(random.random() < self.density):
 
+  def print_road(self):
 
-
-    def print_road(self):
-        print(self.road)
