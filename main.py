b7d7059d1e73e2ae553e6101419eabb4b5e4bb47
wei xie <444980834@qq.com>
2017-11-07 17:13:57 -0500

    fix bug

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 class traffic:
     def __init__(self, n=100,max_v=5,init_density=0.03):
+        self.n=n
         self.road=np.zeros(n)
         self.max_v=max_v
         self.init_density=init_density
@@ -9,8 +10,8 @@ class traffic:
         self.printroad()
 
     def generate_car(self):
-        for i in range(len(self.n)):
-            if(random.random<self.init_density):
+        for i in range(self.n):
+            if(random.random()<self.init_density):
                 self.road[i]=1
 
     # def iteration(self):
---
+++ script.py
@@ -0,0 +1,2 @@
+import main
+a=main.traffic()
