d444ed68b6f81803ce6718c74c76acdc1078f516
wei xie <444980834@qq.com>
2017-12-11 10:43:59 -0500

    change name

--- onelane.py
+++ onelane.py
@@ -2,29 +2,31 @@ import numpy as np
 import random
 
 class traffic:
-    def __init__(self, n=100,max_v=5,density=0.5,slow_down_random_probability=0.1):
+    def __init__(self, n=100,max_v=5,density=0.5,slow_down_random_probability=0.1,generate_car=2):
         self.n=n
         self.cars={}
         self.nextcars={}
         self.max_v=max_v
         self.density=density
         self.slow_down_random_probability=slow_down_random_probability
-        self.generate_car()
+        all_generate_car={1:self.generate_car1,2:self.generate_car2}
+        all_generate_car[generate_car]()
         self.printroad()
 
 
-    def generate_car(self):
+    def generate_car2(self):
         allcar=range(self.n)
         random.shuffle(allcar)
         for i in allcar[:int(self.n*self.density)]:
             self.cars[i]=random.randint(0,self.max_v)
         self.current_car_position=sorted(self.cars.keys())
 
-    # def generate_car(self):
-    #     for i in range(self.n):
-    #         if(i%(self.n/(self.n*self.density))==0):
-    #             self.cars[i]=random.randint(0,self.max_v)
-    #     self.current_car_position=sorted(self.cars.keys())
+    def generate_car1(self):
+        for i in range(self.n):
+            print(self.n,(self.n*self.density))
+            if(i%(self.n/(self.n*self.density))==0):
+                self.cars[i]=random.randint(0,self.max_v)
+        self.current_car_position=sorted(self.cars.keys())
 
     # def generate_car(self):
     #     for i in range(self.n):
